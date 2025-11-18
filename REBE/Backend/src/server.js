7e99cab12f8d1d4d9a7d8fccabf66c11db25e3bb
Malik Bazil Rabbani <asterion911ts@gmail.com>
2025-11-18 14:27:10 +0700

    Fixed dotenv config. Should be working as intended now...

--- REBE/Backend/src/server.js
+++ REBE/Backend/src/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 const Jwt = require('@hapi/jwt');
 
@@ -14,8 +16,8 @@ const ClientError = require('./exceptions/ClientError');
 
 const init = async () => {
   const server = Hapi.server({
-    port: 3000,
-    host: 'localhost',
+    port: process.env.PORT || 3000,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
